bdadc353a2017b837fdc89971b3700d628159e4e
Sarah Connor <sconnor@pivotal.io>
2019-07-21 00:26:10 +0100

    Convert resource tests to ginkgo tests

--- go.mod
+++ go.mod
@@ -1,1 +1,8 @@
 module github.com/terminatingcode/exhibit-resource
+
+require (
+	github.com/cloudboss/ofcourse v0.2.1
+	github.com/onsi/ginkgo v1.8.0
+	github.com/onsi/gomega v1.5.0
+	github.com/stretchr/testify v1.2.2
+)
--- resource/resource_test.go
+++ resource/resource_test.go
@@ -1,4 +1,4 @@
-package resource
+package resource_test
 
 import (
 	"encoding/json"
@@ -6,163 +6,126 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"testing"
 
 	oc "github.com/cloudboss/ofcourse/ofcourse"
-	"github.com/stretchr/testify/assert"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/terminatingcode/exhibit-resource/resource"
 )
 
 var (
 	testLogger = oc.NewLogger(oc.SilentLevel)
+	env        = oc.NewEnvironment()
+	r          = Resource{}
 )
 
-func TestCheck(t *testing.T) {
-	env := oc.NewEnvironment()
-	var testCases = []struct {
-		sourceIn    oc.Source
-		versionIn   oc.Version
-		versionsOut []oc.Version
-		err         error
-	}{
-		{
-			oc.Source{},
-			nil,
-			[]oc.Version{oc.Version{"count": "1"}},
-			nil,
-		},
-		{
-			oc.Source{},
-			oc.Version{"count": "1000"},
-			[]oc.Version{oc.Version{"count": "1001"}},
-			nil,
-		},
-		{
-			oc.Source{},
-			oc.Version{"numero": "1"},
-			nil,
-			ErrVersion,
-		},
-	}
+var _ = Describe("Resource", func() {
+	Describe("Check", func() {
+		Context("with no previous version", func() {
+			It("should increment", func() {
+				versions, err := r.Check(oc.Source{}, nil, env, testLogger)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(versions).To(Equal([]oc.Version{oc.Version{"count": "1"}}))
+			})
+		})
 
-	for _, tc := range testCases {
-		r := Resource{}
-		versions, err := r.Check(tc.sourceIn, tc.versionIn, env, testLogger)
-		assert.Equal(t, tc.versionsOut, versions)
-		assert.Equal(t, tc.err, err)
-	}
-}
+		Context("with a previous version", func() {
+			It("should increment", func() {
+				versions, err := r.Check(oc.Source{}, oc.Version{"count": "1000"}, env, testLogger)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(versions).To(Equal([]oc.Version{oc.Version{"count": "1001"}}))
+			})
+		})
 
-func TestIn(t *testing.T) {
-	env := oc.NewEnvironment()
-	var testCases = []struct {
-		sourceIn    oc.Source
-		paramsIn    oc.Params
-		versionIn   oc.Version
-		metadataOut oc.Metadata
-		err         error
-	}{
-		{
-			oc.Source{},
-			oc.Params{},
-			oc.Version{"count": "1"},
-			oc.Metadata{
-				{Name: "a", Value: "b"}, {Name: "c", Value: "d"},
-			},
-			nil,
-		},
-		{
-			oc.Source{},
-			oc.Params{},
-			oc.Version{"count": "1234"},
-			oc.Metadata{
-				{Name: "a", Value: "b"}, {Name: "c", Value: "d"},
-			},
-			nil,
-		},
-	}
+		Context("with an invalid version", func() {
+			It("should error", func() {
+				versions, err := r.Check(oc.Source{}, oc.Version{"numero": "1"}, env, testLogger)
+				Expect(err).To(HaveOccurred())
+				Expect(versions).To(BeNil())
+			})
+		})
+	})
 
-	for _, tc := range testCases {
-		td, err := ioutil.TempDir("", "resource-")
-		assert.Nil(t, err)
-		defer os.RemoveAll(td)
+	Describe("In", func() {
+		var (
+			td  string
+			err error
+		)
 
-		r := Resource{}
-		version, metadata, err := r.In(td, tc.sourceIn, tc.paramsIn, tc.versionIn, env, testLogger)
+		BeforeEach(func() {
+			td, err = ioutil.TempDir("", "resource-")
+			Expect(err).ToNot(HaveOccurred())
+		})
 
-		// First test the return values
-		assert.Equal(t, tc.versionIn, version)
-		assert.Equal(t, tc.metadataOut, metadata)
-		assert.Equal(t, tc.err, err)
+		AfterEach(func() {
+			defer os.RemoveAll(td)
+		})
 
-		// Ensure the output file has been created
-		path := fmt.Sprintf("%s/version", td)
-		_, fileErr := os.Stat(path)
-		assert.False(t, os.IsNotExist(fileErr))
+		Context("with a valid input", func() {
+			It("should return metatdata", func() {
+				version := oc.Version{"count": "1"}
+				_, metadata, err := r.In(td, oc.Source{}, oc.Params{}, version, env, testLogger)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(metadata).To(ConsistOf(oc.Metadata{{Name: "a", Value: "b"}, {Name: "c", Value: "d"}}))
 
-		// Read the output file
-		bytes, err := ioutil.ReadFile(path)
-		assert.Nil(t, err)
+				path := fmt.Sprintf("%s/version", td)
+				_, fileErr := os.Stat(path)
+				Expect(os.IsNotExist(fileErr)).To(BeFalse())
 
-		// Ensure the contents are as expected
-		var readVersion oc.Version
-		err = json.Unmarshal(bytes, &readVersion)
-		assert.Equal(t, tc.versionIn, readVersion)
-	}
-}
+				bytes, err := ioutil.ReadFile(path)
+				Expect(err).ToNot(HaveOccurred())
 
-func TestOut(t *testing.T) {
-	env := oc.NewEnvironment()
-	var testCases = []struct {
-		sourceIn    oc.Source
-		paramsIn    oc.Params
-		versionOut  oc.Version
-		metadataOut oc.Metadata
-		err         error
-	}{
-		{
-			oc.Source{},
-			oc.Params{},
-			nil,
-			nil,
-			ErrParam,
-		},
-		{
-			oc.Source{},
-			oc.Params{"version_path": "hey/version"},
-			oc.Version{"count": "4567"},
-			oc.Metadata{},
-			nil,
-		},
-	}
+				var readVersion oc.Version
+				err = json.Unmarshal(bytes, &readVersion)
+				Expect(readVersion).To(Equal(version))
+			})
+		})
+	})
 
-	for _, tc := range testCases {
-		// Make a temp dir to be the input directory
-		td, err := ioutil.TempDir("", "resource-")
-		assert.Nil(t, err)
-		defer os.RemoveAll(td)
+	Describe("Out", func() {
+		var (
+			td  string
+			err error
+		)
 
-		// If version_path passed in params, create the version file there
-		versionPath, ok := tc.paramsIn["version_path"].(string)
-		if ok {
-			fullVersionPath := fmt.Sprintf("%s/%s", td, versionPath)
+		BeforeEach(func() {
+			td, err = ioutil.TempDir("", "resource-")
+			Expect(err).ToNot(HaveOccurred())
+		})
 
-			// Create any leading subdirectories in version path
-			err = os.MkdirAll(filepath.Dir(fullVersionPath), 0777)
-			assert.Nil(t, err)
+		AfterEach(func() {
+			defer os.RemoveAll(td)
+		})
 
-			// Marshal the version map to JSON
-			contents, err := json.Marshal(tc.versionOut)
-			assert.Nil(t, err)
+		Context("with a valid version path", func() {
+			It("should return metatdata", func() {
+				version := oc.Version{"count": "1"}
 
-			// Write JSON to file
-			err = ioutil.WriteFile(fullVersionPath, []byte(contents), 0666)
-			assert.Nil(t, err)
-		}
+				versionPath := "path/version"
+				fullVersionPath := fmt.Sprintf("%s/%s", td, versionPath)
+				err = os.MkdirAll(filepath.Dir(fullVersionPath), 0777)
+				Expect(err).ToNot(HaveOccurred())
 
-		r := Resource{}
-		version, metadata, err := r.Out(td, tc.sourceIn, tc.paramsIn, env, testLogger)
-		assert.Equal(t, tc.versionOut, version)
-		assert.Equal(t, tc.metadataOut, metadata)
-		assert.Equal(t, tc.err, err)
-	}
-}
+				contents, err := json.Marshal(version)
+				Expect(err).ToNot(HaveOccurred())
+
+				err = ioutil.WriteFile(fullVersionPath, []byte(contents), 0666)
+				Expect(err).ToNot(HaveOccurred())
+
+				versionOut, metadata, err := r.Out(td, oc.Source{}, oc.Params{"version_path": versionPath}, env, testLogger)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(version).To(Equal(versionOut))
+				Expect(metadata).To(ConsistOf(oc.Metadata{}))
+			})
+		})
+
+		Context("with an invalid version path", func() {
+			It("should error", func() {
+				versionOut, metadata, err := r.Out(td, oc.Source{}, oc.Params{}, env, testLogger)
+				Expect(err).To(HaveOccurred())
+				Expect(versionOut).To(BeNil())
+				Expect(metadata).To(BeNil())
+			})
+		})
+	})
+})
